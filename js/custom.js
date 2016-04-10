11cbeded04ee6f0005cddc98adf54b90bd038a4b
Ben Kissi <benjamin.kissi@meltwater.com>
2016-04-10 17:59:20 +0000

    changed to normal ajax

--- js/custom.js
+++ js/custom.js
@@ -50,47 +50,26 @@ $(document).ready(function(){
     }
 
 
-    // function postToMailChimp() {
-    //     console.log($("#subscribe").serialize())
-    //     $.ajax({
-    //         url: "http://cocktailinsights.us13.list-manage.com/subscribe/post?u=73417957500a00eeeed9ac22e&amp;id=dca066121c&c=?" ,
-    //         data: { 
-    //             "email_address": emailinput,
-    //             "status":"pending",
-    //             "merge_fields":{
-    //                 "FNAME":fname
-    //             }
-    //         },
-    //         dataType: "json",
-    //         contentType: "application/json; charset=utf-8",
-    //         statusCode: {
-    //             404: function() {
-    //                 alert("page not found");
-    //             },
-    //             200: function() {
-    //                 thankyou.show( "slow", function() {
-    //                     thankyou.delay(3000).hide(1000);
-    //                 });
-    //                 emailinput = "";
-    //             }
-    //         }
-    //     });
-    // }
-
-    function postToMailChimp($form) {
-        var $form = $('#subscribe');
+    function postToMailChimp() {
         $.ajax({
-            type: $form.attr('method'),
-            url: $form.attr('action'),
-            data: $form.serialize(),
-            cache       : false,
-            dataType    : 'json',
+            url: "https://us13.api.mailchimp.com/3.0/lists/d572aeae8a/members",
+            headers:{
+                Authorization: 'apikey f874f3a198d224e89df4f5e3fb7a7f55-us13'
+            },
+            data: { 
+                "email_address": emailinput,
+                "status":"pending",
+                "merge_fields":{
+                    "FNAME":fname
+                }
+            },
+            dataType: "json",
             contentType: "application/json; charset=utf-8",
-            error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
-            success     : function(data) {
-                if (data.result != "success") {
-                    alert("ooops");
-                } else {
+            statusCode: {
+                404: function() {
+                    alert("page not found");
+                },
+                200: function() {
                     thankyou.show( "slow", function() {
                         thankyou.delay(3000).hide(1000);
                     });
@@ -99,6 +78,29 @@ $(document).ready(function(){
             }
         });
     }
+
+    // function postToMailChimp($form) {
+    //     var $form = $('#subscribe');
+    //     $.ajax({
+    //         type: $form.attr('method'),
+    //         url: $form.attr('action'),
+    //         data: $form.serialize(),
+    //         cache       : false,
+    //         dataType    : 'json',
+    //         contentType: "application/json; charset=utf-8",
+    //         error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+    //         success     : function(data) {
+    //             if (data.result != "success") {
+    //                 alert("ooops");
+    //             } else {
+    //                 thankyou.show( "slow", function() {
+    //                     thankyou.delay(3000).hide(1000);
+    //                 });
+    //                 emailinput = "";
+    //             }
+    //         }
+    //     });
+    // }
 
 });
 
